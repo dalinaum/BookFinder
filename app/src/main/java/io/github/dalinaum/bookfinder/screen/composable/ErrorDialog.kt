5e5db3a30bb1fc45dcec1187f9f993499641dcd4
LYK <dalinaum@gmail.com>
2023-04-27 19:04:15 +0900

    에러 다이얼로그의 모양을 수정

--- app/src/main/java/io/github/dalinaum/bookfinder/screen/composable/ErrorDialog.kt
+++ app/src/main/java/io/github/dalinaum/bookfinder/screen/composable/ErrorDialog.kt
@@ -1,8 +1,8 @@
 package io.github.dalinaum.bookfinder.screen.composable
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -11,9 +11,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ErrorDialog(
@@ -28,13 +29,20 @@ fun ErrorDialog(
             onDismissRequest = {
             },
             text = {
-                Text(
-                    text = "에러가 발생했습니다.\n다시 시도해주세요.\n\n$message",
-                    modifier = Modifier
-                        .padding(20.dp)
-                        .fillMaxWidth()
-                        .wrapContentWidth(Alignment.CenterHorizontally)
-                )
+                Column {
+                    Text(
+                        text = "에러가 발생했습니다.\n다시 시도해주세요.",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(
+                        modifier = Modifier.size(8.dp)
+                    )
+                    Text(
+                        text = message,
+                        fontSize = 16.sp
+                    )
+                }
             },
             title = {
                 Text("에러")
