fb845da9b9a63f18ff3eb2fda33c6077a4f9daec
LYK <dalinaum@gmail.com>
2023-04-27 19:17:12 +0900

    엔터 키 처리를 하게 하고 색상을 변경

--- app/src/main/java/io/github/dalinaum/bookfinder/screen/composable/SearchField.kt
+++ app/src/main/java/io/github/dalinaum/bookfinder/screen/composable/SearchField.kt
@@ -1,5 +1,6 @@
 package io.github.dalinaum.bookfinder.screen.composable
 
+import android.view.KeyEvent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -8,13 +9,17 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.unit.dp
 import io.github.dalinaum.bookfinder.ui.theme.WColorLight
+import io.github.dalinaum.bookfinder.ui.theme.WColorLight3
 
 @Composable
 fun SearchField(
@@ -35,13 +40,28 @@ fun SearchField(
                 onValueChange = onQueryChanged,
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
-                    .weight(1f),
+                    .weight(1f)
+                    .onKeyEvent {
+                        if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
+                            onSearchButtonClicked()
+                            true
+                        } else {
+                            false
+                        }
+                    },
+
                 label = {
                     Text(
                         text = "검색어를 입력해주세요."
                     )
                 },
-                singleLine = true
+                singleLine = true,
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = WColorLight3,
+                    focusedIndicatorColor = Color.LightGray,
+                    unfocusedIndicatorColor = Color.DarkGray,
+                    cursorColor = Color.Black
+                )
             )
             IconButton(
                 onClick = onSearchButtonClicked,
